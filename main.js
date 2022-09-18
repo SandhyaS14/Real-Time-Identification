0882c4e72c6ee591991d738cc5696e97deb0092c
SandhyaS14 <85371666+SandhyaS14@users.noreply.github.com>
2022-09-18 13:30:10 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+noseX = 0;
+noseY = 0;
+leftX = 0;
+rightX = 0;
+difference = 0;
+
+function setup() {
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+
+    canvas = createCanvas(550, 525);
+    canvas.position(560, 150);
+
+    posenet = ml5.poseNet(video, modelLoaded);
+    posenet.on("pose", gotResult);
+}
+
+function modelLoaded() {
+    console.log("Posenet is loaded");
+}
+
+function draw() {
+    background("grey");
+    fill("blue");
+    stroke("blue");
+    sqaure(noseX, noseY, 100);
+}
+
+function gotResult(results) {
+    if(results.length > 0) {
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        leftX = results[0].pose.leftwrist.x;
+        rightX = results[0].pose.rightwrist.x;
+        difference = rightX - leftX;
+        console.log(difference);
+    }
+}
