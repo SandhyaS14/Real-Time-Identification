cc8f62391a1a0261d0b22d84c0ac0337ea8784fc
SandhyaS14 <85371666+SandhyaS14@users.noreply.github.com>
2022-09-18 13:44:56 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -32,8 +32,8 @@ function gotResult(results) {
         console.log(results);
         noseX = results[0].pose.nose.x;
         noseY = results[0].pose.nose.y;
-        leftX = results[0].pose.leftwrist.x;
-        rightX = results[0].pose.rightwrist.x;
+        leftX = results[0].pose.leftWrist.x;
+        rightX = results[0].pose.rightWrist.x;
         difference = floor(rightX - leftX);
         console.log(difference);
     }
